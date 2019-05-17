17c355aa141a8ab33afec96386fbd80bb906611e
nicolasgozul <47230124+nicolasgozul@users.noreply.github.com>
2019-05-17 08:22:47 -0500

    Add files via upload

---
+++ MovCamara.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovCamara : MonoBehaviour
+{
+
+    Vector2 touchDeltaPosition;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            float pointer_x = Input.GetAxis("Mouse X");
+            float pointer_y = Input.GetAxis("Mouse Y");
+            transform.Translate(-pointer_x * 0.5f,
+                        -pointer_y * 0.5f, 0);
+        }
+    }
+}
